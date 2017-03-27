76e90376771f9034ce3f6e676595414069878323
David Urbina <davidcurbina@gmail.com>
2017-03-27 18:29:04 -0400

    debugging

--- index.php
+++ index.php
@@ -8,6 +8,8 @@ function __autoload($file_name){
 		echo "Files Loaded";
 	} else if (file_exists('./Controllers/'.$file_name.'.php')){
 		require_once('/Controllers/'.$file_name.'.php');
+	} else {
+		echo "Could not find app/Classes";
 	}
 }
 ?>
