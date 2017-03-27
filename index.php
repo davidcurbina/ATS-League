4636be78fa5010b568d8c3470589a8e15375b3f3
David Urbina <davidcurbina@gmail.com>
2017-03-27 18:36:24 -0400

    login view not showing

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require('routes.php');
 
 //Function to AutoLoad all classes and controllers
 function __autoload($file_name){
+	echo $filename."</br>";
 	if(file_exists('Classes/'.$file_name.'.php')){
 		require_once('Classes/'.$file_name.'.php');
 		echo "Class Files Loaded";
@@ -10,7 +11,7 @@ function __autoload($file_name){
 		require_once('Controllers/'.$file_name.'.php');
 		echo "Controller Files Loaded";
 	} else {
-		echo "Could not find Classes";
+		echo "Could not find file";
 	}
 }
 ?>
