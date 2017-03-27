40bafa7a5768cba0e3ebce308c063d883d5a853f
David Urbina <davidcurbina@gmail.com>
2017-03-27 18:30:24 -0400

    debugging

--- index.php
+++ index.php
@@ -3,13 +3,13 @@ require('routes.php');
 
 //Function to AutoLoad all classes and controllers
 function __autoload($file_name){
-	if(file_exists('/Classes/'.$file_name.'.php')){
-		require_once('/Classes/'.$file_name.'.php');
+	if(file_exists('Classes/'.$file_name.'.php')){
+		require_once('Classes/'.$file_name.'.php');
 		echo "Files Loaded";
-	} else if (file_exists('/Controllers/'.$file_name.'.php')){
-		require_once('/Controllers/'.$file_name.'.php');
+	} else if (file_exists('Controllers/'.$file_name.'.php')){
+		require_once('Controllers/'.$file_name.'.php');
 	} else {
-		echo "Could not find /Classes";
+		echo "Could not find Classes";
 	}
 }
 ?>
