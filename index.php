a7c300e985028d25fa1c00d84685de7d9311d9ea
David Urbina <davidcurbina@gmail.com>
2017-03-27 17:50:02 -0400

    Edited Proc File to change application base to app/

--- index.php
+++ index.php
@@ -3,10 +3,10 @@ require('routes.php');
 
 //Function to AutoLoad all classes and controllers
 function __autoload($file_name){
-	if(file_exists('./app/Classes/'.$file_name.'.php')){
-		require_once('./app/Classes/'.$file_name.'.php');
-	} else if (file_exists('./app/Controllers/'.$file_name.'.php')){
-		require_once('./app/Controllers/'.$file_name.'.php');
+	if(file_exists('./Classes/'.$file_name.'.php')){
+		require_once('./Classes/'.$file_name.'.php');
+	} else if (file_exists('./Controllers/'.$file_name.'.php')){
+		require_once('./Controllers/'.$file_name.'.php');
 	}
 }
 ?>
