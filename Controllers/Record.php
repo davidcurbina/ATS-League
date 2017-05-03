e67125a9653f38bd4cbf02e7b0bb36370da69a35
David Urbina <davidcurbina@gmail.com>
2017-05-03 18:45:27 -0400

    Added action paramter in create_record endpoint

--- Controllers/Record.php
+++ Controllers/Record.php
@@ -5,16 +5,26 @@ class Record{
     $record = json_decode($record);
     $emp_id = Database::escape_data($record->emp_id);
     $type = Database::escape_data($record->type);
+    $type = Database::escape_data($record->type);
+    $action = Database::escape_data($record->action);
     $date = date('Y-m-d H:i:s');
     $date = date('Y-m-d H:i:s');
     
     
     if($exists = Database::select("Select * from employees where id =".$emp_id)){
+      if($action  == "in"){
         if(Database::modify("Insert into records (emp_id,type,check_in) Values (".$emp_id.",'".$type."','".$date."');")){
             echo "Success";
         } else{
             echo "Error: Insert data failed";
         }
+      }else {
+        if(Database::modify("Insert into records (emp_id,type,check_out) Values (".$emp_id.",'".$type."','".$date."');")){
+            echo "Success";
+        } else{
+            echo "Error: Insert data failed";
+        }
+      }
         //echo "Insert into records (emp_id,type,check_in) Values (".$emp_id.",'".$type."','".$date."');";
     } else {
         echo "Employee not found";
@@ -23,7 +33,7 @@ class Record{
   
   public static function GetEmployees(){
     
-    if($exists = Database::select("Select * from employees")){
+    if($exists = Database::select("Select id from employees")){
         echo json_encode($exists);
     } else {
         echo "Employee not found";
